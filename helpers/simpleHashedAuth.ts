fdd695be734641b4d349ca2ff36c8164fca0a4d6
Felipe Lecot <47688222+FelipeLecot@users.noreply.github.com>
2023-11-04 17:00:17 -0300

    Update simpleHashedAuth.ts

--- helpers/simpleHashedAuth.ts
+++ helpers/simpleHashedAuth.ts
@@ -1,12 +1,16 @@
-import { pbkdf2 } from "crypto";
+import { pbkdf2 } from 'crypto';
 
-const iterations = 100000; // Replace iterations, make random? 
+export const hashPassword = (
+	password: string,
+	saveHashData: (hash: string, salt: string) => void
+) => {
+	const salt = crypto.getRandomValues(new Uint8Array(128)).toString();
+	return pbkdf2(password, salt, 10000, 64, 'sha512', (err, deriverKey) => saveHashData(deriverKey.toString(), salt));
+};
 
-export const hashPassword = (password: string, saveHashData: (hash: string, salt: string) => void) => {
-  const salt = crypto.getRandomValues(new Uint8Array(128)).toString();
-  pbkdf2(password, salt, iterations, 64, 'sha512', (err, deriverKey) => {saveHashData(deriverKey.toString(), salt)});
-}
-
-export const isPasswordCorrect = (savedSalt: string, passwordAttempt: string, checkPassword: (hash: string) => void) => {
-  pbkdf2(passwordAttempt, savedSalt, iterations, 64, 'sha512', (err, deriverKey) => {checkPassword(deriverKey.toString())});
-}
+export const isPasswordCorrect = (
+	savedSalt: Buffer,
+	passwordAttempt: string,
+) => new Promise((resolve, reject) => {
+	pbkdf2(passwordAttempt, savedSalt, 10000, 64, 'sha512', (err, deriverKey) => resolve(deriverKey.toString()));
+})
